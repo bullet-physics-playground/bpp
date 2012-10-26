6628b15580949793351bf66b1cae096e80d8f97f
Jaime Vives Piqueres <jaime@ignorancia.org>
2012-10-26 14:32:11 +0300

    Update src/objects/cylinder.cpp
    + changed shape to match glut shape orientation
    + also, depth should be half, as the params are "halfextents"
    + added inertia calculation instead passing just mass
    + translate GL cylinder to match Bullet cylinder origin
    + add proper cylinder SDL to the export

--- src/objects/cylinder.cpp
+++ src/objects/cylinder.cpp
@@ -33,11 +33,7 @@ void Cylinder::init(btScalar width, btScalar height, btScalar depth,
   lengths[1] = height;
   lengths[2] = depth;
 
-  // -------------------
-  // changed shape to match glut shape orientation
-  // also, depth should be half, as the params are "halfextents"
   shape = new btCylinderShapeZ(btVector3(width, height, depth*.5));
-  // -------------------
 
   btQuaternion qtn;
   btTransform trans;
@@ -49,7 +45,10 @@ void Cylinder::init(btScalar width, btScalar height, btScalar depth,
   trans.setOrigin(btVector3(0,0,0));			
   motionState = new btDefaultMotionState(trans);
 
-  body = new btRigidBody(mass, motionState, shape, btVector3(mass, mass, mass));
+  btVector3 inertia;
+  shape->calculateLocalInertia(mass,inertia);
+  body = new btRigidBody(mass, motionState, shape, inertia);
+  
 }
 
 void Cylinder::luaBind(lua_State *s) {
@@ -95,10 +94,8 @@ void Cylinder::renderInLocalFrame(QTextStream *s) const
 
   glPushMatrix();
   glMultMatrixf(m);
-  // -------------------
   // translate to match Bullet cylinder origin
   glTranslated(0,0,-lengths[2]*.5);
-  // -------------------
   glScalef(lengths[0], lengths[1], lengths[2]);
   
   glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
@@ -113,7 +110,7 @@ void Cylinder::renderInLocalFrame(QTextStream *s) const
 
   //TODO  
   if (s != NULL) {
-    *s << "box { <" << -lengths[0]/2.0 << ", " << -lengths[1]/2.0 << ", " << -lengths[2]/2.0 << ">, <" << lengths[0]/2.0 << ", " << lengths[1]/2.0 << ", " << lengths[2]/2.0 << ">\n      pigment { rgb <" << color[0]/255.0 << ", " << color[1]/255.0 << ", " << color[2]/255.0 << "> }" << endl;
+    *s << "cylinder { " << -lengths[2]/2.0 << "*z, " << lengths[2]/2.0 << "*z, " << lengths[0]/2.0 << "\n      pigment { rgb <" << color[0]/255.0 << ", " << color[1]/255.0 << ", " << color[2]/255.0 << "> }" << endl;
     *s <<  "  matrix <" << m[0] << "," << m[1] << "," << m[2] << ",\n        " << m[4] << "," << m[5] << "," << m[6] << ",\n        " << m[8] << "," << m[9] << "," << m[10] << ",\n        " << m[12] << "," << m[13] << "," << m[14] << ">" << endl;
     *s << "}" << endl << endl;
   }
