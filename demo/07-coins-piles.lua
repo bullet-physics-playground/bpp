ff4984e47a1c1687d798e8954ed93cada48e33d2
jaimevives <jaime@ignorancia.org>
2012-11-06 18:12:44 +0100

    updated demo script to show use of custom-sdl properties

--- demo/07-coins-piles.lua
+++ demo/07-coins-piles.lua
@@ -9,7 +9,28 @@ plane.friction=.7
 
 v:add(plane)
 
-function coins_pile(coin_width,coin_height,N,xp,zp)
+function coins_pile(coin_type,N,xp,zp)
+
+  if(coin_type==1) then
+    coin_width=7.0
+    coin_height=1.1
+  end
+  if(coin_type==2) then
+    coin_width=6.0
+    coin_height=1.0
+  end
+  if(coin_type==3) then
+    coin_width=5.0
+    coin_height=0.9
+  end
+  if(coin_type==4) then
+    coin_width=4.0
+    coin_height=0.8
+  end
+  if(coin_type==5) then
+    coin_width=3.5
+    coin_height=0.75
+  end
 
   for i = 0,N do
     q = btQuaternion(1,0,0,1)
@@ -20,18 +41,20 @@ function coins_pile(coin_width,coin_height,N,xp,zp)
     d.trans=btTransform(q,o)
     d.friction=.4
     d.restitution=.1
+    d.pre_sdl="object{coin("..coin_type..")"
+    d.post_sdl="}\n"
     v:add(d)
   end
 end
 
-coins_pile(6.0,1.0,45,0,15)
-coins_pile(5.0,0.9,41,0,-15)
-coins_pile(7.0,1.1,43,15,0)
-coins_pile(4.0,0.8,48,-15,0)
-coins_pile(3.5,.75,46,0,0)
-coins_pile(6.0,1.0,45,15,15)
-coins_pile(5.0,0.9,41,15,-15)
-coins_pile(7.0,1.1,43,-15,15)
-coins_pile(4.0,0.8,48,-15,-15)
+coins_pile(1,45,0,15)
+coins_pile(2,41,0,-15)
+coins_pile(3,43,15,0)
+coins_pile(4,48,-15,0)
+coins_pile(5,46,0,0)
+coins_pile(1,45,15,15)
+coins_pile(2,41,15,-15)
+coins_pile(3,43,-15,15)
+coins_pile(4,48,-15,-15)
 
 -- EOF
