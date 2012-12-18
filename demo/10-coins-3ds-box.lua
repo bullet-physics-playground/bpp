ee10f5e2241132de98cc05036e63c3a0fcaad110
jaimevives <jaime@ignorancia.org>
2012-12-18 22:49:39 +0100

    updated coins demo, using postSim() now

--- demo/10-coins-3ds-box.lua
+++ demo/10-coins-3ds-box.lua
@@ -1,27 +1,23 @@
+-- postSim used to insert coins on-the-fly
+
 plane = Plane(0,1,0)
 plane.col = "#111111"
 v:add(plane)
-
-function coins_pile(num_obj,pos)
- for i=1,num_obj do
-  c=Cylinder(7,7,1.1,1)
-  q = btQuaternion(1,0,0,1)
-  o = btVector3(pos.x,pos.y+i*2,pos.z)
-  c.trans=btTransform(q,o)  
-  c.col="#00ffff"
-  c.friction=.5
-  c.resitution=.1
-  v:add(c)
- end
-end
-
-coins_pile(50,btVector3(0,2,0))
-coins_pile(50,btVector3(14.5,2,0))
-coins_pile(50,btVector3(0,2,14.5))
-coins_pile(50,btVector3(-14.5,2,0))
-coins_pile(50,btVector3(0,2,-14.5))
 
 b=Mesh3DS("demo/box.3ds",0)
 b.pos=btVector3(0,20.15,0)
 b.col="#ff0000"
 v:add(b)
+
+v:postSim(function(N)
+if(N%5==0) then
+  c=Cylinder(7,7,1.1,1)
+  q = btQuaternion(1,0,0,1)
+  o = btVector3(math.random(-2,2),150,math.random(-2,2))
+  c.trans=btTransform(q,o)  
+  c.col="#00ffff"
+  c.friction=.5
+  c.resitution=.1
+  v:add(c)
+end
+end)
