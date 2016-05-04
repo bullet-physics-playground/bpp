6f41696e2acb6555bc0ea9d74b0607a23a2c24e7
Jakob Flierl <jakob.flierl@gmail.com>
2016-05-05 00:36:27 +0200

    Add pov-ec2.sh setup script for Ubuntu 14.04 on Amazon EC2

---
+++ tools/pov-ecs-ubuntu-14.04.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Install
+#
+# POV-Ray setup with LightsysIV, ffmpeg and youtube-upload on Ubuntu 14.04.
+#
+# see: https://github.com/bullet-physics-playground/bpp/issues/8
+
+sudo apt-get -y install povray htop uptimed screen vim unzip make
+
+cd
+mkdir .povray
+cd .povray
+wget -c http://www.ignorancia.org/uploads/zips/lightsys4d.zip
+unzip lightsys4d.zip
+sudo su -c 'echo "Library_Path=\"/home/ubuntu/.povray/LightsysIV\"" >> /etc/povray/3.7/povray.ini'
+cd
+
+sudo add-apt-repository -y ppa:mc3man/trusty-media
+sudo apt-get update
+sudo apt-get -y install ffmpeg
+
+sudo apt-get -y install python-dev
+sudo pip install --upgrade google-api-python-client progressbar2
+
+wget https://github.com/tokland/youtube-upload/archive/master.zip
+unzip master.zip
+cd youtube-upload-master
+sudo python setup.py install
