ba2f57cd33e7ef62e0db16e8ab82b2edb04dac4b
Jakob Flierl <jakob.flierl@gmail.com>
2016-06-05 17:27:47 +0200

    Add docker-bpp-bash.sh

---
+++ tools/docker-bpp-bash.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# Starts an interactive shell inside the docker container that is used to build the Debian package
+#
+
+docker run -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -it travis.debian.net/bpp /bin/bash
