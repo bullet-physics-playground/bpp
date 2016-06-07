dad222e6d0fe9288954a09c71812746cd515da86
Jakob Flierl <jakob.flierl@gmail.com>
2016-06-07 16:34:33 +0200

    Add xhost local:root

--- tools/docker-ubuntu-bash.sh
+++ tools/docker-ubuntu-bash.sh
@@ -2,5 +2,5 @@
 #
 # Starts an interactive shell inside the docker container that is used to build the Debian package
 #
-
+xhost local:root
 docker run -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -it travis.ubuntu.com/bpp /bin/bash
