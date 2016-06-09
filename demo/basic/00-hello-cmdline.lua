ea8ae28a83af27bdd5f5c70d60d5b921b671ad84
Jakob Flierl <jakob.flierl@gmail.com>
2016-06-09 17:29:38 +0200

    Misc

--- demo/basic/00-hello-cmdline.lua
+++ demo/basic/00-hello-cmdline.lua
@@ -4,23 +4,25 @@
 
 -- You can run this Lua script from the command-line:
 --
---   $ bpp -n 60 -f 00-hello-cmdline.lua
+--   $ bpp -n 200 -f 00-hello-cmdline.lua
 --
 -- Or plot the result with gnuplot:
 --
---   $ bpp -n 60 -f 00-hello-cmdline.lua | \
+--   $ bpp -n 200 -f 00-hello-cmdline.lua | \
 --     gnuplot -e "set terminal dumb; plot for[col=3:3] '/dev/stdin' using 1:col title columnheader(col) with lines"
 
 -- a fixed plane in the x-z dimension
 p = Plane(0,1,0,0,100)
-p.col = "gray"
+p.col = "white"
+p.restitution = 1
 v:add(p)
 
 -- a sphere with diameter 1 and mass 10
-s = Sphere(1,10)
-s.pos = btVector3(0,10,0) -- position
-s.vel = btVector3(10,0,0) -- velocity
-s.col = "green"
+s = Sphere(0.5,10)
+s.pos = btVector3( 0,10, 0) -- position
+s.vel = btVector3( 2, 0, 0) -- velocity
+s.col = "red"
+s.restitution =.725
 v:add(s)
 
 v:preSim(function(N)
