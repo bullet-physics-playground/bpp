cdc78a316d75a2c481e34510918e4e0a3f39be2c
Jakob Flierl <jakob.flierl@gmail.com>
2021-03-26 09:04:50 +0100

    Add joystick interface callback.

--- demo/basic/01-callback.lua
+++ demo/basic/01-callback.lua
@@ -79,3 +79,22 @@ v:postSim(function(N)
   end
 
 end)
+
+-- Called 25 times per second,
+-- when joystick is present
+v:onJoystick(function(N, joy)
+  v:clearDebugText()
+  print("onJoystick("..tostring(N)..")")
+  print("axes:")
+--  for i = 1, #joy.axes do
+--    print(i.." "..tostring(joy.axes[i]))
+--  end
+  for i, axis in ipairs(joy.axes) do
+    print("* "..i..": "..axis)
+  end
+  print("buttons:")
+  for i, f in ipairs(joy.buttons) do
+    print("* "..i..": "..tostring(f))
+  end
+  --print(tostring(joy.axes[1]))
+end)
