bad063375bfcd31bb9640a896bf452b6ae7f7912
Jakob Flierl <jakob.flierl@gmail.com>
2026-05-14 20:40:21 +0200

    Beginning of an inverted pendulum simulation

--- demo/constraint/03-slider.lua
+++ demo/constraint/03-slider.lua
@@ -5,18 +5,43 @@
 --
 
 plane = Plane(0,1,0,0,10)
+plane.pos = btVector3(0, -3, 0)
 plane.col = "#222222"
-v:add(plane)
+--v:add(plane)
 
 base = Cube(15,0.5,2,0)
 base.pos = btVector3(0, 0.5, 0)
 base.col = "#444444"
 v:add(base)
 
-slider = Cube(5,0.5,2,1)
+slider = Cube(1.5,0.5,2,1)
 slider.pos = btVector3(0, 1, 0)
 slider.col = "#ff6600"
 v:add(slider)
+
+-- Inverted Pendulum
+pendulum = Cylinder(0.2, 3, 1)
+pendulum.pos = btVector3(0, 3, 2)
+pendulum.col = "#00ff00"
+v:add(pendulum)
+
+-- Second Pendulum Cylinder
+pendulum2 = Cylinder(0.2, 4, 1)
+pendulum2.pos = btVector3(0, 3, 4)
+pendulum2.col = "#0000ff"
+v:add(pendulum2)
+
+pendulum_con = btHingeConstraint(
+  slider.body, pendulum.body,
+  btVector3(0, 0.5, 1), btVector3(0, 0, -1),
+  btVector3(0, 0, 1), btVector3(0, 0, 1))
+v:addConstraint(pendulum_con)
+
+pendulum_con2 = btHingeConstraint(
+  pendulum.body, pendulum2.body,
+  btVector3(0, 0, 1), btVector3(0, 0, -2.5),
+  btVector3(0, 1, 0), btVector3(0, 2, 0))
+v:addConstraint(pendulum_con2)
 
 frameInA = btTransform()
 frameInA:setIdentity()
