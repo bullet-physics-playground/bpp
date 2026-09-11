350de981b898981e43db301d3d377deca87c7b38
Jakob Flierl <jakob.flierl@gmail.com>
2026-09-11 15:22:39 +0200

    Fix github-release.sh tag naming and interactive commit

--- scripts/github-release.sh
+++ scripts/github-release.sh
@@ -2,7 +2,7 @@
 
 # TYPE= argument to the script where 0 = MAJOR, 1 = MINOR, 2 = BUILD. Default to BUILD.
 GIT_VERSION=$(git describe --tags)
-CURRENT_VERSION=$(echo ${GIT_VERSION:1} | cut -d'-' -f1)
+CURRENT_VERSION=$(echo ${GIT_VERSION} | cut -d'-' -f1)
 TYPE=${1:-2}
 
 function increment_version() {
@@ -20,8 +20,10 @@ sed -i -- "s/PROJECT_NUMBER = .*/PROJECT_NUMBER = \"$NEW_VERSION\"/" Doxyfile
 sed -i -- "s/\!define VERSION.*/\!define VERSION \"$NEW_VERSION\"/" bpp.nsi
 sed -i -- "s/#define APP_VERSION QString(\".*\")/\#define APP_VERSION QString(\"$NEW_VERSION\")/" src/main.cpp
 
-dch -v $NEW_VERSION && git commit -p -m "Bump version $NEW_VERSION" && git tag v${NEW_VERSION}
-git commit
-git push origin tag v${NEW_VERSION}
-
-gh release create v${NEW_VERSION} --generate-notes
+dch -v $NEW_VERSION &&
+git add Doxyfile bpp.nsi src/main.cpp debian/changelog &&
+git commit -m "Bump version $NEW_VERSION" &&
+git tag $NEW_VERSION &&
+git push origin master &&
+git push origin tag $NEW_VERSION &&
+gh release create $NEW_VERSION --generate-notes
